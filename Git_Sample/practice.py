89926def54bd4e3a639bdaedd17fb68b6e080336
Eswar39 <osurieswar@gmail.com>
2021-04-05 16:28:49 +0530

    practice file update in git

---
+++ Git_Sample/practice.py
@@ -0,0 +1,12 @@
+for n in range(1,20):
+    for i in range(1,n):
+        if i%n==0:
+            print("Wierd")
+            if i <5 :
+                print("Not Wierd")
+            elif i<20:
+                print("Wierd")
+            elif i < 20:
+                print("Wierd")
+                
+            
